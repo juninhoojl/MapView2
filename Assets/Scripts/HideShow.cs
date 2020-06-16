caa82226739be71c941b032e197110fefa69c0ad
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-06-16 10:58:54 +0200

    LinhaCharge

--- Assets/Scripts/HideShow.cs
+++ Assets/Scripts/HideShow.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HideShow : MonoBehaviour
-{
+{   
+
+    public LineRenderer linhaPertoBike;
+    public LineRenderer linhaPertoCharge;
+
     public void ShowObjects(bool valor){
 
         GameObject[] poiList = GameObject.FindGameObjectsWithTag("poiBike");
@@ -14,9 +18,11 @@ public class HideShow : MonoBehaviour
             if(!valor){
                  //f.SetActive(false);
                 f.GetComponent<Renderer>().enabled = false;
+                linhaPertoBike.GetComponent<Renderer>().enabled = false;
             }else{
                //  f.SetActive(true);
                 f.GetComponent<Renderer>().enabled = true;
+                linhaPertoBike.GetComponent<Renderer>().enabled = true;
             }
             
         }
@@ -33,9 +39,11 @@ public class HideShow : MonoBehaviour
             if(!valor){
                  //f.SetActive(false);
                 f.GetComponent<Renderer>().enabled = false;
+                linhaPertoCharge.GetComponent<Renderer>().enabled = false;
             }else{
                //  f.SetActive(true);
                 f.GetComponent<Renderer>().enabled = true;
+                linhaPertoCharge.GetComponent<Renderer>().enabled = false;
             }
             
         }
